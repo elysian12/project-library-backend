7973e937a1d6d976a3ffe7f501cad65f08fde1e6
elysian12 <elysian1232@gmail.com>
2021-12-23 21:43:54 +0530

    completed the authrication flow

---
+++ errors/index.js
@@ -0,0 +1,8 @@
+const CustomAPIError = require('./custom-error')
+const BadRequest = require('./bad_request')
+const UnauthenticatedError = require('./unauthenticated')
+
+
+module.exports ={
+    CustomAPIError,BadRequest,UnauthenticatedError
+}
--- routes/main.js
+++ routes/main.js
@@ -3,9 +3,10 @@ const express = require('express')
 const router = express.Router();
 const {login,dashBoard} =require('../controller/main')
 
+const authMiddleware = require('../middleware/auth')
 
 router.route('/login').post(login)
-router.route('/dashboard').get(dashBoard)
+router.route('/dashboard').get(authMiddleware,dashBoard)
 
 
 module.exports = router
